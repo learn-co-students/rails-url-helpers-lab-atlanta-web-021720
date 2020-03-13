1b025233def5c82bf378fddd24b0ae4bb164b00f
laarchambault <laarchambault@gmail.com>
2020-03-13 13:52:56 -0400

    progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "John", last_name: "Kirkland")
+Student.create(first_name: "Tommy", last_name: "Sandefur")
+Student.create(first_name: "Chris", last_name: "Strasburger")
